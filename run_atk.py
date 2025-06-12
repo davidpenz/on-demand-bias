d54b327c961858b92cafaaf8aeb0b5958f13d728
davidpenz <45949304+davidpenz@users.noreply.github.com>
2025-06-12 22:03:56 +0200

    Update run_atk.py

--- run_atk.py
+++ run_atk.py
@@ -12,10 +12,3 @@ if __name__ == "__main__":
     training_fn = run_atk_train_test
 
     run_atk_experiments(params, max_jobs, training_fn)
-# %%
-# from src.utils.input_validation import find_rec_models
-
-# exp = "/media/gustavo/Storage/dummy_debiasrec/ml-1m/RegMultVAE--2024-11-11_17-15-04"
-# find_rec_models(exp)
-
-# %%
